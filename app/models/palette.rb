58ee5a0775001bd172b6ef5b3b2a2082e4d38aca
Jacob Atamian <jpatamian@gmail.com>
2018-04-25 14:43:11 -0400

    pull req updates

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -1,2 +1,5 @@
 class Palette < ApplicationRecord
+  validates :title, null: false
+  validates :hexcode, null: false 
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,11 @@
 
 palette_attributes = [
   { title: "Fall Leaves", description: "blah blah blah", hexcode: "#d54700" },
-  { title: "Just Red", hexcode: "#ff0000"}
+  { title: "Just Red", hexcode: "#ff0000"},
+  { title: "Marine", description: "Swim The Sea", hexcode: "#00FFFF"},
+  { title: "Autumnal", description: "Autumn Love", hexcode: "#ff8800" },
+  { title: "Grassy", hexcode: "#0d6300" }
+
 ]
 
 palette_attributes.each do |a|
