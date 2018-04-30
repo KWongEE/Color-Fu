9341e3e7f4c49a0c22237226d2c8e2c4f63bb6a9
Kevin <kwongca.95@gmail.com>
2018-04-30 17:09:52 -0400

    Color Picker and CSS

--- app/javascript/react/App.js
+++ app/javascript/react/App.js
@@ -5,13 +5,14 @@ import PalettesIndexContainer from './containers/PalettesIndexContainer'
 import PaletteShowContainer from './containers/PaletteShowContainer'
 import NavBar from './components/NavBar'
 import PaletteFormContainer from './containers/PaletteFormContainer'
+import ColorPicker from './components/ColorPicker'
 
 const App = (props) => {
   return(
     <Router history={browserHistory}>
       <Route path='/'  component = {NavBar}>
         <IndexRoute component={PalettesIndexContainer} />
-        <Route path='/palettes/new' component={PaletteFormContainer} />
+        <Route path='/palettes/new' component={ColorPicker} />
         <Route path="/palettes/:id" component={PaletteShowContainer}/>
         <Route path="*" component={PalettesIndexContainer} />
       </Route>
