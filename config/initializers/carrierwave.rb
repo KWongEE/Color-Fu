94efe4ac6d382bb48018f2854d0aa50131212e3a
Kevin <kwongca.95@gmail.com>
2018-05-02 15:29:22 -0400

    remove AWS keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,10 +8,7 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['AKIAJAXAT4HDUALCL6RA'],
-      :aws_secret_access_key => ENV['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'],
-      :region => 'eu-east-1'
+
     }
     config.fog_directory = ENV['colorfuphoto']
   else
