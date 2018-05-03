fda4d2eb777c66033d3ae59c118c19a58fbb19f9
Kevin <kwongca.95@gmail.com>
2018-05-03 14:35:25 -0400

    Finalize merge

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,13 +8,9 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-<<<<<<< HEAD
       provider: 'AWS',
       aws_access_key_id: ENV["AWS_ID"],
       aws_secret_access_key: ENV["AWS_KEY"]
-=======
-      
->>>>>>> fdb4af4c1343c1f6bfa090705d869c7687c5b067
     }
     config.fog_directory = ENV['colorfuphoto']
   else
