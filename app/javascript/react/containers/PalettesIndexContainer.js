d9e69989af819fc9fa6a316ff554a3dac90fb616
Ancient Mariner <albatross@Nots-MacBook-Pro.local>
2018-05-04 13:03:19 -0400

    Search and Styling

--- app/javascript/react/containers/PalettesIndexContainer.js
+++ app/javascript/react/containers/PalettesIndexContainer.js
@@ -5,28 +5,51 @@ class PalettesIndexContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      palettes: []
+      allPalettes: [],
+      palettes: [],
+      search: '',
+      finalResults: []
     }
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
-    fetch('api/v1/palettes')
-      .then(response => {
-        if (response.ok) {
-          return response
-        } else {
-          let errorMessage = `${response.status} (${response.statusText})`
-        }
+   fetch('api/v1/palettes')
+     .then(response => {
+       if (response.ok) {
+         return response
+       } else {
+         let errorMessage = `${response.status} (${response.statusText})`
+       }
+     })
+     .then(response => response.json())
+     .then(paletteObjs => {
+       this.setState({
+         allPalettes: paletteObjs.palettes,
+         palettes: paletteObjs.palettes
+        })
+     })
+     .catch(error => console.error(`${error.message}`))
+  }
+
+  handleChange(event) {
+    let formPayload = {search: event.target.value}
+    let searchResults = []
+    let search = formPayload.search.toLowerCase();
+
+    this.state.allPalettes.forEach((palette) => {
+      if (palette["title"].toLowerCase().includes(search)) {
+       searchResults.push(palette)
+     }
+      this.setState({
+        palettes: searchResults,
+        search: event.target.value
       })
-      .then(response => response.json())
-      .then(paletteObjs => {
-        this.setState( { palettes: paletteObjs.palettes } )
-      })
-      .catch(error => console.error(`${error.message}`))
+    })
   }
 
   render() {
-    let palettes = this.state.palettes.map(palette => {
+    let finalResults = this.state.finalResults.map(palette => {
       return(
         <PaletteTile
           key = {palette.id}
@@ -37,12 +60,37 @@ class PalettesIndexContainer extends Component {
         />
       )
     })
+    let palettes = this.state.palettes.map(palette => {
+      return(
+        <PaletteTile
+            key = {palette.id}
+            id = {palette.id}
+            title = {palette.title}
+            description = {palette.description}
+            hexcodes = {palette.hexcodes}
+        />
+      )
+    })
 
     return(
-      <div className="row">
-        {palettes}
-      </div>
-    )
+     <div className="index-container">
+       <div className="columns large-12 small-12 medium-12">
+        <div className="searchbar small-12 large-12">
+           <input
+             id="search"
+             type='text'
+             value={this.state.search}
+             onChange={this.handleChange}
+             placeholder="Discover Palettes"
+           />
+        </div>
+       </div>
+
+       <div className="row">
+         {palettes}
+       </div>
+     </div>
+   )
   }
 }
 
